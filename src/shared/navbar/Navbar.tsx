2af9d38f031213b26adf16b8acfe6a44122a0a94
David92p <Dave.panetta92@gmail.com>
2023-12-05 21:19:06 +0100

    Add section control gs

--- src/shared/navbar/Navbar.tsx
+++ src/shared/navbar/Navbar.tsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 import logo from "../../assets/logo.png"
+import it from "../../assets/it.png"
+import en from "../../assets/en.png"
 import { RxHamburgerMenu } from "react-icons/rx";
 import{ AiOutlineClose } from "react-icons/ai";
+import { MdDarkMode, MdSunny  } from "react-icons/md";
 import { Link, NavLink } from "react-router-dom";
 import "./style.css";
+import { useStoreSelector, useStoreDispatch } from "../store/hooks"
+import globalSlice from "../../data/appSlice"
 
 const Navbar: React.FC = () => {
+  const darkMode:boolean = useStoreSelector((state) => state.globalReducer.darkMode)
 
+  const dispatch = useStoreDispatch()
   const [menu, setMenu] = useState<boolean>(false);
 
   const onMenu = () => {
@@ -46,11 +53,41 @@ const Navbar: React.FC = () => {
             onClick={() => onMenu()}
           />
         )}
+        
       </div>
       <div className="container-link absolute top-[-100%] 2xl:relative 2xl:top-[100%]">
         <NavLink to="/">Home</NavLink>
         <NavLink to="start">Start</NavLink>
         <NavLink to="contact">Contact</NavLink>
+      </div>
+      <div className="flex justify-around 2xl:justify-between items-center 2xl:w-80 py-4">
+        <div className="flex gap-4">
+          <img
+            onClick={() => dispatch(globalSlice.actions.changeLanguage("ENG"))}
+            src={en}
+            alt="english"
+            className="w-8 h-8 sm:w-10 sm:h-10 rounded-full cursor-pointer"
+          />
+          <img
+            onClick={() => dispatch(globalSlice.actions.changeLanguage("ITA"))}
+            src={it}
+            alt="italian"
+            className="w-8 h-8 sm:w-10 sm:h-10 rounded-full cursor-pointer"
+          />
+        </div>
+        <label
+          className={`bg-amber-300 relative w-16 h-8 rounded-full`}
+        >
+          <input
+            onClick={() => dispatch(globalSlice.actions.changeDarkMode(!darkMode))}
+            type="checkbox"
+            aria-label="set dark mode"
+            className="sr-only peer "
+          />
+          <span className="flex justify-center items-center w-2/5 h-4/5 bg-white absolute rounded-full left-1 top-1 cursor-pointer peer-checked:bg-white peer-checked:left-8 transition-all duration-500">
+            {darkMode ? <MdDarkMode/> : <MdSunny />}
+          </span>
+        </label>
       </div>
     </div>
   );

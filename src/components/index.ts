85131d19e559b06f2ec28b23b386f05b40aee5f1
David92p <dave.panetta92@gmail.com>
2023-12-05 17:11:44 +0100

    Added import default file

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import Home from "./home/Home";
+import Start from "./start/Start";
+import Contact from "./contact/Contact";
+import ErrorServer from "./errorServer/ErrorServer";
+
+export { Home, Start, Contact, ErrorServer }
---
+++ src/shared/index.ts
@@ -0,0 +1,6 @@
+import store from "./store/store"
+import Navbar from "./navbar/Navbar"
+import RoutesNav from "./navbar/RoutesNav"
+
+
+export { store, Navbar, RoutesNav }

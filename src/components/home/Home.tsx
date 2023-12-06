97050439db40be9514c79fe89c06869f9869015c
David92p <dave.panetta92@gmail.com>
2023-12-06 17:03:37 +0100

    Added img 3 home

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import home1 from "../../assets/home1.jpg"
 import home2 from "../../assets/home2.jpg"
+import home3 from "../../assets/home3.jpg"
 import { useStoreSelector } from "../../shared/store/hooks";
 
 const Home:React.FC = () => {
@@ -24,6 +25,12 @@ const Home:React.FC = () => {
 				<img src={home2} alt="Home Img" className='w-44 md:w-72 2xl:w-96 rounded-full 2xl:ml-10 mb-4 shadow-xl shadow-amber-900/100'/>
 				<div className="leading-8 2xl:leading-10 2xl:ml-40">
 					{state.par2}
+				</div>
+			</div>
+			<div className='flex flex-col sm:flex-row justify-center items-center mb-10 mt-8 mx-4 2xl:mx-0 text-justify 2xl:w-[80%]'>
+				<img src={home3} alt="Home Img" className='w-44 md:w-72 2xl:w-96 rounded-full 2xl:mr-10 mb-4 shadow-xl shadow-amber-900/100'/>
+				<div className="leading-8 2xl:leading-10 2xl:mr-40">
+					{state.par1}
 				</div>
 			</div>
     </div>

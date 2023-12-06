a270bebc20803849d48447407722db0bd1c248ac
David92p <dave.panetta92@gmail.com>
2023-12-06 13:16:34 +0100

    fixed bg

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { useStoreSelector } from "./shared/store/hooks";
 function App() {
   const darkMode:boolean = useStoreSelector((state) => state.globalReducer.darkMode)
   return (
-    <div className={`${darkMode ? "bg-amber-500" : "bg-amber-200"} min-h-screen`}>
+    <div className={`${darkMode ? "bg-yellow-700" : "bg-amber-200"} min-h-screen`}>
       <BrowserRouter>
 				<Navbar/>
 				<RoutesNav/>

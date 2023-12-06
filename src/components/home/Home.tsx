88fba8e308346eaab342e30a226b5c15fb32cb2e
David92p <dave.panetta92@gmail.com>
2023-12-06 13:17:14 +0100

    fixed syle and added data from state global

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -5,21 +5,26 @@ import { useStoreSelector } from "../../shared/store/hooks";
 
 const Home:React.FC = () => {
 	const navigate = useNavigate();
-	const language:string = useStoreSelector((state) => state.globalReducer.language)
+	const state = useStoreSelector((state) => state.globalReducer)
   return (
-    <div className='w-full flex flex-col'>
-			<div className="h-24 flex justify-center items-center border-4 border-white">
-				<button onClick={() => navigate("start")} className="bg-amber-900 hover:bg-amber-800 px-8 py-2 rounded-2xl text-white font-medium tracking-wider hover:tracking-widest cursor-pointer">START</button>
+    <div className='w-full flex flex-col 2xl:items-center'>
+			<div className="w-full text-center py-10 px-2 tracking-widest text-xl 2xl:text-2xl font-semibold">
+				{state.header}
 			</div>
-			<div className='border-8 border-red-800 flex flex-col sm:flex-row justify-center items-center mx-4 mb-10 text-justify gap-10'>
-				<img src={home1} alt="Home Img" className='w-44 md:w-72 2xl:w-96 rounded-full mt-10'/>
-				<h1 className="leading-8">
-					{language == "ENG" ? "HELLOooooooo" : "La meditazione è una pratica divenuta di recente sempre più popolare: se ne parla nell’ambito dello yoga, della mindfulness, delle tecniche di rilassamento o di gestione dello stress, ma la sua storia è antichissima e affonda le radici nelle tradizioni religiose e spirituali di tutto il mondo. "}
-				</h1>
+			<div className="h-24 flex justify-center items-center">
+				<button onClick={() => navigate("start")} className="bg-amber-900 hover:bg-amber-800 px-8 py-2 rounded-2xl text-white font-medium tracking-wider hover:tracking-widest transition duration-300 ease-in-out shadow-xl shadow-amber-700/50">START</button>
 			</div>
-			<div className='flex flex-col sm:flex-row-reverse justify-center items-center mx-4 text-justify gap-10'>
-				<img src={home2	} alt="Home Img" className='w-44 md:w-72 2xl:w-96 rounded-full mt-10'/>
-				<h1 className="leading-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat veritatis cupiditate inventore voluptates rerum consectetur vero modi quasi ratione placeat quis voluptatum sapiente enim eum, debitis libero quisquam ad maxime.</h1>
+			<div className='flex flex-col sm:flex-row justify-center items-center mb-10 mt-8 mx-4 2xl:mx-0 text-justify 2xl:w-[80%]'>
+				<img src={home1} alt="Home Img" className='w-44 md:w-72 2xl:w-96 rounded-full 2xl:mr-10 mb-4 shadow-xl shadow-amber-900/100'/>
+				<div className="leading-8 2xl:leading-10 2xl:mr-40">
+					{state.par1}
+				</div>
+			</div>
+			<div className='flex flex-col sm:flex-row-reverse justify-center items-center mb-10 mt-8 mx-4 2xl:mx-0 text-justify 2xl:w-[80%]'>
+				<img src={home2} alt="Home Img" className='w-44 md:w-72 2xl:w-96 rounded-full 2xl:ml-10 mb-4 shadow-xl shadow-amber-900/100'/>
+				<div className="leading-8 2xl:leading-10 2xl:ml-40">
+					{state.par2}
+				</div>
 			</div>
     </div>
   )

0e443cae334b7e0f0a74f3a07f72772fdf8d0c78
David92p <dave.panetta92@gmail.com>
2023-12-12 17:49:18 +0100

    Fixed color bg

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -7,7 +7,7 @@ import { useStoreSelector } from "../../shared/store/hooks";
 const About:React.FC = () => {
   const state = useStoreSelector((state) => state.globalReducer)
 	return (
-    <div className={`w-full flex flex-col 2xl:items-center ${state.darkMode ? "bg-slate-800 text-white" : "bg-amber-200 text-slate-900"} `}>
+    <div className={`w-full flex flex-col 2xl:items-center ${state.darkMode ? "bg-stone-800 text-white" : "bg-amber-200 text-slate-900"} `}>
 			<div className='flex flex-col sm:flex-row justify-center items-center mb-10 mt-8 2xl:mt-14 mx-4 2xl:mx-0 text-justify 2xl:w-[80%]'>
 				<img src={about1} alt="Home Img" className='w-44 md:w-72 2xl:w-96 rounded-full 2xl:mr-10 mb-4 shadow-xl shadow-amber-900/100'/>
 				<div className="leading-8 2xl:leading-10 2xl:mr-40">

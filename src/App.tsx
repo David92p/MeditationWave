b1428ac3fa8e989dcc86312d5db6c405dea2b432
David92p <dave.panetta92@gmail.com>
2023-12-12 10:54:01 +0100

    Removed container

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import { Navbar, RoutesNav } from "./shared";
-import { useStoreSelector } from "./shared/store/hooks";
 
 function App() {
-  const darkMode:boolean = useStoreSelector((state) => state.globalReducer.darkMode)
   return (
-    <div className={`${darkMode ? "bg-yellow-700" : "bg-amber-200"} min-h-screen`}>
-      <BrowserRouter>
-				<Navbar/>
-				<RoutesNav/>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+			<Navbar/>
+			<RoutesNav/>
+    </BrowserRouter>
   )
 }
 

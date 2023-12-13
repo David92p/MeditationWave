8bf7a5380c03754bffe0299e23ea61220e2da246
David92p <dave.panetta92@gmail.com>
2023-12-13 16:45:28 +0100

    fixed about data

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -8,28 +8,28 @@ const About:React.FC = () => {
   const state = useStoreSelector((state) => state.globalReducer)
 	return (
     <div className={`w-full flex flex-col 2xl:items-center ${state.darkMode ? "bg-stone-800 text-white" : "bg-amber-200 text-slate-900"} `}>
-			<div className='flex flex-col sm:flex-row justify-center items-center mb-10 mt-8 2xl:mt-14 mx-4 2xl:mx-0 text-justify 2xl:w-[80%]'>
-				<img src={about1} alt="Home Img" className='w-44 md:w-72 2xl:w-96 rounded-full 2xl:mr-10 mb-4 shadow-xl shadow-amber-900/100'/>
+			<div className='flex flex-col sm:flex-row justify-center items-center mb-10 mt-8 2xl:mt-14 mx-4 sm:mx-10 2xl:mx-0 text-justify 2xl:w-[80%]'>
+				<img src={about1} alt="Home Img" className='w-44 md:w-72 2xl:w-96 rounded-full sm:mr-10 mb-4 shadow-xl shadow-amber-900/100'/>
 				<div className="leading-8 2xl:leading-10 2xl:mr-40">
-					{state.home.par1}
+					{state.about.par1}
 				</div>
 			</div>
-			<div className='flex flex-col sm:flex-row-reverse justify-center items-center mb-10 mt-8 mx-4 2xl:mx-0 text-justify 2xl:w-[80%]'>
-				<img src={about2} alt="Home Img" className='w-44 md:w-72 2xl:w-96 rounded-full 2xl:ml-10 mb-4 shadow-xl shadow-amber-900/100'/>
+			<div className='flex flex-col sm:flex-row-reverse justify-center items-center mb-10 mt-8 mx-4 sm:mx-10 2xl:mx-0 text-justify 2xl:w-[80%]'>
+				<img src={about2} alt="Home Img" className='w-44 md:w-72 2xl:w-96 rounded-full sm:ml-10 mb-4 shadow-xl shadow-amber-900/100'/>
 				<div className="leading-8 2xl:leading-10 2xl:ml-40">
-					{state.home.par2}
+					{state.about.par2}
 				</div>
 			</div>
-			<div className='flex flex-col sm:flex-row justify-center items-center mb-10 mt-8 mx-4 2xl:mx-0 text-justify 2xl:w-[80%]'>
-				<img src={about3} alt="Home Img" className='w-44 md:w-72 2xl:w-96 rounded-full 2xl:mr-10 mb-4 shadow-xl shadow-amber-900/100'/>
+			<div className='flex flex-col sm:flex-row justify-center items-center mb-10 mt-8 mx-4 sm:mx-10 2xl:mx-0 text-justify 2xl:w-[80%]'>
+				<img src={about3} alt="Home Img" className='w-44 md:w-72 2xl:w-96 rounded-full sm:mr-10 mb-4 shadow-xl shadow-amber-900/100'/>
 				<div className="leading-8 2xl:leading-10 2xl:mr-40">
-					{state.home.par3}
+					{state.about.par3}
 				</div>
 			</div>
-      <div className='flex flex-col sm:flex-row-reverse justify-center items-center mb-10 mt-8 mx-4 2xl:mx-0 text-justify 2xl:w-[80%]'>
-				<img src={about4} alt="Home Img" className='w-44 md:w-72 2xl:w-96 rounded-full 2xl:ml-10 mb-4 shadow-xl shadow-amber-900/100'/>
+      <div className='flex flex-col sm:flex-row-reverse justify-center items-center mb-10 mt-8 mx-4 sm:mx-10 2xl:mx-0 text-justify 2xl:w-[80%]'>
+				<img src={about4} alt="Home Img" className='w-44 md:w-72 2xl:w-96 rounded-full sm:ml-10 mb-4 shadow-xl shadow-amber-900/100'/>
 				<div className="leading-8 2xl:leading-10 2xl:ml-40">
-					{state.home.par4}
+					{state.about.par4}
 				</div>
 			</div>
     </div>

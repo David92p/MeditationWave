18bd64028dfb1ef8a491c67b4cd1b05fe6956214
David92p <dave.panetta92@gmail.com>
2023-12-18 14:13:13 +0100

    Fixed imports

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,6 +2,5 @@ import { EngHome } from "./context/context";
 import { ItaHome } from "./context/context";
 import { EngAbout } from "./context/context";
 import { ItaAbout } from "./context/context";
-import { audio } from "./audio/audio"
 
-export { EngHome, ItaHome, EngAbout, ItaAbout, audio }
+export { EngHome, ItaHome, EngAbout, ItaAbout }
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,4 +1,4 @@
-import store from "./store/store"
+import store from "../store/store"
 import Navbar from "./navbar/Navbar"
 import RoutesNav from "./navbar/RoutesNav"
 

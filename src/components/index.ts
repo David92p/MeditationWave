f374970d4972fbc1787e743495419e1a76d0a1b4
David92p <Dave.panetta92@gmail.com>
2024-01-03 21:24:23 +0100

    added count down import

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,9 +7,10 @@ import Settings from "./start/Settings";
 import BtnSetting from "./start/BtnSetting";
 import PlayPauseStop from "./start/PlayPauseStop";
 import Timer from "./start/Timer";
+import useCountdown from "./start/useCountdown";
 import Footer from "./footer/Footer";
 
-export { Home, About, Contact, ErrorServer, Controller, Settings, BtnSetting, PlayPauseStop, Timer, Footer }
+export { Home, About, Contact, ErrorServer, Controller, Settings, BtnSetting, PlayPauseStop, Timer, useCountdown, Footer }
 
 import type { Controls } from "./start/Controller"
 export type { Controls }

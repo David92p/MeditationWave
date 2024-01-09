86152bb485eb1a725080d0a5735aceb47529662c
David92p <dave.panetta92@gmail.com>
2024-01-09 17:32:34 +0100

    removed component import

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,10 +7,9 @@ import Settings from "./start/Settings";
 import BtnSetting from "./start/BtnSetting";
 import PlayPauseStop from "./start/PlayPauseStop";
 import Timer from "./start/Timer";
-import useCountdown from "./start/useCountdown";
 import Footer from "./footer/Footer";
 
-export { Home, About, Contact, ErrorServer, Controller, Settings, BtnSetting, PlayPauseStop, Timer, useCountdown, Footer }
+export { Home, About, Contact, ErrorServer, Controller, Settings, BtnSetting, PlayPauseStop, Timer, Footer }
 
 import type { Controls } from "./start/Controller"
 export type { Controls }

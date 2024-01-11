db67881e7f21cd26cb2c6f9aa50d4b3e8e6a57c8
David92p <dave.panetta92@gmail.com>
2024-01-11 13:32:59 +0100

    Fixed import

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,7 @@ import Home from "./home/Home";
 import About from "./about/About";
 import Contact from "./contact/Contact";
 import ErrorServer from "./errorServer/ErrorServer";
-import Controller from "./start/Controller";
+import Controller from "./start/controller/Controller";
 import Settings from "./start/Settings";
 import BtnSetting from "./start/BtnSetting";
 import PlayPauseStop from "./start/PlayPauseStop";
@@ -11,5 +11,5 @@ import Footer from "./footer/Footer";
 
 export { Home, About, Contact, ErrorServer, Controller, Settings, BtnSetting, PlayPauseStop, Timer, Footer }
 
-import type { Controls } from "./start/Controller"
+import type { Controls } from "./start/controller/Controller"
 export type { Controls }
